709b59851d6f5d57ff312bc2b5ec4b7157660420
Michelle <Michelle.Tinajero@outlook.com>
2020-05-17 21:27:57 -0500

    added styling

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useHistory } from 'react-router-dom';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
+import './Login.scss';
 
 const Login = (props) => {
   // make a post request to retrieve a token from the api
@@ -35,10 +36,11 @@ const Login = (props) => {
   }
 
   return (
-    <>
-     <h1>Please Login to view this super cool hex color website</h1>
-     <form onSubmit={handleSubmit}>
+    <div className='wrapperClass'>
+     <h1 className='headerClass'>Please login to view super cool hex colors!</h1>
+     <form className="formClass" onSubmit={handleSubmit}>
        <input 
+          className="inputClass"
           type='text'
           name='username'
           placeholder='Username'
@@ -47,16 +49,17 @@ const Login = (props) => {
         />
 
         <input 
+          className="inputClass"
           type='password'
           name='password'
           placeholder='Password'
           value={login.password} 
           onChange={handleChange}
         />
-
-        <button>Login</button>
+        
+        <button class="pulse-button">Login</button>
      </form>
-    </>
+    </div>
   );
 };
 
